bd2eb3f65d937dd8ada16692eade8c84545386a1
William Faircloth <willuhmjs@gmail.com>
2023-02-23 16:48:53 +0000

    chore: create project skeleton, add draggable item

---
+++ src/lib/Item.svelte
@@ -0,0 +1,22 @@
+<style>
+    .item-container {
+        border: 1px solid black;
+        width: 80%;
+        margin: auto;
+        padding: 1rem;
+        background-color: skyblue;
+    }
+
+    
+</style>
+<script lang="ts">
+    import { draggable } from '@neodrag/svelte';
+	import { element } from 'svelte/internal';
+    import { onMount } from 'svelte';
+
+    
+    
+</script>
+<div use:draggable={{ bounds: 'body' }} id="item" class="item-container">
+    Hello!
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,36 @@
+<style>
+    .container {
+        display: flex;
+        flex-direction: row;
+        justify-content: space-around;
+    }
+
+    .drag-area {
+        width: 30%;
+        height: 100vh;
+        border-left: 1px solid black;
+        border-right: 1px solid black;
+    }
+</style>
+<script lang="ts">
+        import Item from '../lib/Item.svelte';
+        import { onMount } from 'svelte';
+        onMount(() => {
+            const item = document.getElementById('item') || new Element();
+            item.addEventListener('onclick', ((event) => {
+            //do something
+            console.log(event);
+            }) as EventListener);
+    })
+</script>
+<div class="container">
+    <div class="drag-area" >
+        <Item/>
+    </div>
+    <div class="drag-area">
+
+    </div>
+    <div class="drag-area">
+
+    </div>
+</div>
