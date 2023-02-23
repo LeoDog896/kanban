f9f5926be647c6167a3cc4edf2401a123715b71c
William Faircloth <willuhmjs@gmail.com>
2023-02-23 20:36:45 +0000

    fix: removal trivial code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,14 +8,6 @@
 <script lang="ts">
         import Item from '../lib/Item.svelte';
         import DragArea from '../lib/DragArea.svelte';
-        import { onMount } from 'svelte';
-        onMount(() => {
-            const item = document.getElementById('item') || new Element();
-            item.addEventListener('onclick', ((event) => {
-            //do something
-            console.log(event);
-            }) as EventListener);
-    })
 </script>
 <div class="container">
     <DragArea id="todo-area">
